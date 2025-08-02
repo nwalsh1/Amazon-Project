1e5a4e32913d99af7fffd3683c0c50bcb6b09e65
Neil <78010987+nwalsh1@users.noreply.github.com>
2025-08-02 12:31:24 -0400

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,32 +7,6 @@ Main Idea of Javascript
 
 //save the information about our products in the JavaScript file using an array of objects
 //structure needs to be the same so we can have modularity
-const products = [{
-  image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-
-},{
-  image : 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-},{
-  image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
 
 let productsHTML = '';
 
