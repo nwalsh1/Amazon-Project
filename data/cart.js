c8bfa83c0a2b2837de8eb92d37cbe862766fad74
Neil <78010987+nwalsh1@users.noreply.github.com>
2025-08-12 11:08:41 -0400

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,12 @@
 //Export cart
-export const cart = [];
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', //use the ID to find the product info, just need to save this, dedupe/normalizing the data
+  quantity: 2
+},
+{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
 
 //13m
 let timeout;
